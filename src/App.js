f20ce1ec6142907956b4061c0ba8512ceaddbb79
vivekv <110380885+vivekv2k3@users.noreply.github.com>
2023-11-09 19:44:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ColorPicker from "./components/ColorPicker";
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPicker />
+    </div>
+  );
+}
+
+export default App;
